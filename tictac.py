fefa20465fdc9601e854ce6db2739a60d00e5c54
saidazimov720 <saidazimovnurbek01gmail.com>
2023-12-08 18:07:06 +0500

    grid

--- tictac.py
+++ tictac.py
@@ -13,3 +13,14 @@ square_size = width // grid_size
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Tic-Tac-Toe")
 
+font = pygame.font.Font(None, 74)
+
+board = [['' for _ in range(grid_size)] for _ in range(grid_size)]
+player_turn = 'X'
+winner = None
+game_over = False
+
+def draw_grid():
+    for i in range(1, grid_size):
+        pygame.draw.line(screen, line_color, (i * square_size, 0 ), (i * square_size, height) line_width)
+        pygame.draw.line(screen, line_color, (0, i * square_size), (width i * square_size) line_width)
