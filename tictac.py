62fb47c8f3aab13a54e0f127c97c854818d46ca5
saidazimov720 <saidazimovnurbek01gmail.com>
2023-12-08 18:19:59 +0500

    add the grid

--- tictac.py
+++ tictac.py
@@ -22,5 +22,18 @@ game_over = False
 
 def draw_grid():
     for i in range(1, grid_size):
-        pygame.draw.line(screen, line_color, (i * square_size, 0 ), (i * square_size, height) line_width)
-        pygame.draw.line(screen, line_color, (0, i * square_size), (width i * square_size) line_width)
+        pygame.draw.line(screen, line_color, (i * square_size, 0), (i * square_size, height), line_width)
+        pygame.draw.line(screen, line_color, (0, i * square_size), (width, i * square_size), line_width)
+
+def draw_XO():
+    for row in range(grid_size):
+        for col in range(grid_size):
+            if board[row][col] == 'X':
+                pygame.draw.line(screen, line_color, (col * square_size, row * square_size),
+                                 ((col + 1) * square_size, (row + 1) * square_size), line_width)
+                pygame.draw.line(screen, line_color, ((col + 1) * square_size, row * square_size),
+                                 (col * square_size, (row + 1) * square_size), line_width)
+            elif board[row][col] == 'O':
+                pygame.draw.circle(screen, line_color,
+                                   (int(col * square_size + square_size / 2), int(row * square_size + square_size / 2)),
+                                   int(square_size / 2), line_width)
