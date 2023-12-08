780bc649f284e388c9afe9ccb5b21784ff23164c
saidazimov720 <saidazimovnurbek01gmail.com>
2023-12-08 17:52:53 +0500

    initialization

---
+++ tictac.py
@@ -0,0 +1,11 @@
+import pygame
+import sys
+
+pygame.init()
+
+width, height = 600, 600
+line_color = (0, 0, 0)
+background_color = (255, 255, 255)
+line_width = 15
+grid_size = 3
+square_size = width // grid_size
